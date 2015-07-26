7c83545ddb8d377ffb3686724f1d0a30eeecff23
Matteo Rosati <mrosati@h-art.com>
2015-07-26 13:40:05 +0200

    add office in list_display and set pagination to 500 items

--- www/admin.py
+++ www/admin.py
@@ -4,13 +4,15 @@ from www.models import Company, Brand, Printer
 # Register your models here.
 
 class PrinterAdmin(admin.ModelAdmin):
-    list_display = ('label', 'company', 'area', 'referer', 'brand', 'model', 'is_network_printer')
+    list_display = ('label', 'company', 'area', 'referer', 'office', 'brand', 'model', 'is_network_printer')
     list_filter = ['company']
     fieldsets = [
         ('', { 'fields': ['label', 'company', 'area', 'floor', 'office', 'referer', 'brand', 'model', 'image', 'ip_address'] }),
         ('Prima visita', { 'fields': ['first_visit', 'first_visit_black', 'first_visit_color'] }),
         ('Seconda visita', { 'fields': ['second_visit', 'second_visit_black', 'second_visit_color'] }),
     ]
+
+PrinterAdmin.list_per_page = 500
 
 admin.site.register(Company)
 admin.site.register(Brand)
